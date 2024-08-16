b9d4759dc52dac18c64c94554c12a7634027e2c9
Victor Jelmlin <vigge.jelmlin@hotmail.com>
2024-08-16 23:23:01 +0200

    Hard typing. Very hard

--- src/scenes/test/felix/testscne.gd
+++ src/scenes/test/felix/testscne.gd
@@ -2,18 +2,18 @@ extends Node2D
 
 const DUDE = preload("res://scenes/npc/dude.tscn")
 
-var dudes = []
+var dudes: Array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	call_deferred("test")
 	for i in range(0, 100):
-		var dude = DUDE.instantiate()
+		var dude: Node = DUDE.instantiate()
 		dudes.append(dude)
 		add_child(dude)
 	pass # Replace with function body.
 
-func test():
+func test() -> void:
 	print("baking...")
 	var new_navigation_map: RID = NavigationServer2D.map_create()
 	NavigationServer2D.map_set_active(new_navigation_map, true)
@@ -21,10 +21,10 @@ func test():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		var pos = get_global_mouse_position()
-		for dude in dudes:
+		var pos: Vector2 = get_global_mouse_position()
+		for dude: Node in dudes:
 			dude.set_movement_target(pos)
 		print("moving to: ", pos)
 	pass
