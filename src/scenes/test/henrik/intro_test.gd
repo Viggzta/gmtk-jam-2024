09860fafa2b76f3bd21df52f204b5c1499dc7a2a
Henrik Hedström <henrik.hedstrom@hotmail.com>
2024-08-18 01:37:31 +0200

    boss talky :100:

---
+++ src/scenes/test/henrik/intro_test.gd
@@ -0,0 +1,14 @@
+extends Control
+const BOSS = preload("res://scenes/boss/boss.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var intro_boss : Boss = BOSS.instantiate()
+	intro_boss.initialize("Yooo, congratulations on your new job as mayor!")
+	add_child(intro_boss)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
