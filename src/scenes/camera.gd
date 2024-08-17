610d76e0748bdb6cdfff2dde263a8bfc99a2c103
Victor Jelmlin <vigge.jelmlin@hotmail.com>
2024-08-17 02:34:18 +0200

    Spawn the dudes

--- src/scenes/camera.gd
+++ src/scenes/camera.gd
@@ -1,5 +1,8 @@
 extends Camera2D
 
+func _ready() -> void:
+	zoom = Vector2.ONE * 0.25
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
--- src/scenes/levels/the_level.gd
+++ src/scenes/levels/the_level.gd
@@ -1,9 +1,17 @@
 extends Node2D
 
+const HOUSE = preload("res://scenes/buildings/house.tscn")
+const DUDE = preload("res://scenes/npc/dude.tscn")
+
+@onready var dude_root: Node2D = $DudeRoot
+@onready var building_root: Node2D = $BuildingRoot
+
 @export var buildable_radius: float = 2.5
 @export var building_offset: int = 256
+@export var dude_spawn_offset: float = 50.0
+
 var building_spots: Dictionary = {}
-const HOUSE = preload("res://scenes/buildings/house.tscn")
+var current_max_radius: float;
 
 func _create_build_spots(radius: float) -> void:
 	for x: int in range(-radius, radius + 1):
@@ -15,7 +23,27 @@ func _create_build_spots(radius: float) -> void:
 					var house: Node2D = HOUSE.instantiate()
 					house.position = new_pos
 					building_spots[new_pos] = house
-					add_child(house)
+					building_root.add_child(house)
+	if current_max_radius < radius:
+		current_max_radius = radius
 
 func _ready() -> void:
 	_create_build_spots(buildable_radius)
+	_spawn_wave(50)
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.keycode == KEY_B:
+			_spawn_wave(10)
+	
+func _spawn_wave(amount: int) -> void:
+	print(current_max_radius)
+	for i in range(0, amount):
+		var spawn_location_radians: float = randf() * TAU
+		var spawn_location: Vector2 = Vector2(
+			cos(spawn_location_radians) * current_max_radius * building_offset + cos(spawn_location_radians) * dude_spawn_offset,
+			sin(spawn_location_radians) * current_max_radius * building_offset + sin(spawn_location_radians) * dude_spawn_offset)
+		var dude: Node2D = DUDE.instantiate()
+		dude.current_need = Need.NeedType.Poop
+		dude.position = spawn_location
+		dude_root.add_child(dude)
