87d852205e64a9c1aff93cfceef03172d28e5fc9
Henrik Hedström <henrik.hedstrom@hotmail.com>
2024-08-17 20:48:27 +0200

    *added cinema

--- src/scripts/cinema.gd
+++ src/scripts/cinema.gd
@@ -2,9 +2,9 @@ class_name Cinema extends Building
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	sprite = $Hospital
+	sprite = $Cinema
 	body = $Collider
-	need_type = Dude.NeedType.Eat
+	need_type = Dude.NeedType.Poop
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
