be650636a4c384d0911036a36055d44a2ddcc983
Victor Jelmlin <vigge.jelmlin@hotmail.com>
2024-08-18 15:07:07 +0200

    Add building sounds

---
+++ src/scenes/fx/fire_and_forget_sound.gd
@@ -0,0 +1,25 @@
+class_name FireAndForgetSound extends Node2D
+
+enum SoundClips {
+	Woosh,
+	Uuuh,
+	Suuck,
+}
+
+var _sound_dictionary: Dictionary = {
+	SoundClips.Woosh: preload("res://audio/fx/build_sound.wav"),
+	SoundClips.Uuuh: preload("res://audio/fx/no_build.wav"),
+	SoundClips.Suuck: preload("res://audio/fx/suuuck.wav"),
+}
+
+var _audio_stream: AudioStream
+
+func set_and_play(sound_clip: SoundClips) -> void:
+	_audio_stream = _sound_dictionary[sound_clip]
+	
+func _ready() -> void:
+	$AudioStreamPlayer2D.stream = _audio_stream
+	$AudioStreamPlayer2D.play()
+
+func _on_audio_stream_player_2d_finished() -> void:
+	self.queue_free()
