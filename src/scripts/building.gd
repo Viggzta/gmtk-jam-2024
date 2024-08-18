d144afb38f41adfa8d9c1ffca7f19b20b6627939
Felix Kaaman <tmtu@tmtu.ee>
2024-08-18 14:11:03 +0200

    animate house popin

--- src/scripts/building.gd
+++ src/scripts/building.gd
@@ -21,6 +21,12 @@ func _ready() -> void:
 	body.input_event.connect(_on_clicked)
 	body.mouse_entered.connect(_on_hover)
 	body.mouse_exited.connect(_on_hover_exit)
+	var pos: Vector2 = Vector2(sprite.position.x, sprite.position.y)
+	sprite.position.y -= 150
+	sprite.scale = Vector2(2.5, 2.5)
+	var animation_time: float = 0.3 + randf() * 0.5
+	get_tree().create_tween().tween_property(sprite, "position", Vector2(pos.x, pos.y), animation_time).set_trans(Tween.TRANS_ELASTIC)
+	get_tree().create_tween().tween_property(sprite, "scale", Vector2(1, 1), animation_time).set_trans(Tween.TRANS_ELASTIC)
 	
 func _on_clicked(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event.is_action_pressed("left_mouse_click"):
