260a35911f6a08932952198c2559566d17c2d2cf
Henrik Hedström <henrihed@axis.com>
2024-08-19 20:10:55 +0200

    updates to cut and scissor

--- src/scenes/rope/cut_scene.gd
+++ src/scenes/rope/cut_scene.gd
@@ -1,12 +1,21 @@
-extends Node2D
+extends Control
 @onready var boss: Boss = $Boss
-@onready var scissor: Node2D = $Scissor
-
+@onready var scissor: Scissor = $Scissor
+var has_started_time: bool = false
+var time_after_cut_to_start_game : int = 5
 
 var intro_speech: Array[String] = [
 	"Hello! Welcome to the tourist island!",
 	"Your new job is to be a mayor of this town.",
 	"It's quite easy, just remember to keep the tourists satisfied.",
+	"Now time for the boring stuff:",
+	"Left-click to select a building from the bottom bar.",
+	"Left-click on a house to replace it with the currently selected building.",
+	"Right-click a special building to pick it up.",
+	"Middle-click + drag to pan the camera.",
+	"WASD/←↑↓→ to pan the camera.",
+	"Scroll-wheel to zoom.",
+	"Esc in the island scene brings up volume sliders.",
 	"Go on now, cut the rope!"
 ]
 
@@ -14,4 +23,15 @@ var intro_speech: Array[String] = [
 func _ready() -> void:
 	boss.initialize(intro_speech)
 	boss.start_talking = true
+	boss.talk_completed.connect(_on_boss_finished)
+	
+func _process(delta: float) -> void:
+	if scissor.has_cut and not has_started_time:
+		has_started_time = true
+		await get_tree().create_timer(time_after_cut_to_start_game).timeout
+		print("START GAME")
+	
+func _on_boss_finished() -> void:
+	scissor.visible = true
+	
 	
